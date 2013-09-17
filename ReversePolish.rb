b7dd289d5bb5d4cf3e034f8affa7498558a1df23
RicardoZeballos <z.rick3@berkeley.edu>
2013-09-17 15:31:31 -0700

    First Commit

---
+++ ReversePolish.rb
@@ -0,0 +1,31 @@
+# #!/usr/bin/env ruby
+# Ricardo Zeballos 9/17/2013
+
+print "Welcome to the Reverse Calculator\n"
+stack = []
+operators = ["+","-","*","/"]
+
+class String
+  def is_numeric?
+    begin Float(self) ; true end rescue false
+  end
+end
+
+while(true)
+  print ">"
+  input = gets.chomp
+  if(input == "q")
+    puts "Goodbye"  
+    break
+  elsif(input.is_numeric?)
+    stack.push(input.to_f)
+  elsif(operators.include?(input) && (stack.size >= 2))
+    exp = "#{input} #{stack.pop}"
+    exp = "#{stack.pop} #{exp}"
+    result = instance_eval(exp)
+    stack.push(result)
+  else
+    puts "Error: Not enough numbers left or not valid input."
+  end
+  puts stack.last 
+end
